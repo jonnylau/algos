6d75e690b06f7afb153eb0b450cedba405f9a7da
Jon Lau <jonathanclau8@gmail.com>
2018-03-12 16:06:16 -0700

    completed clojure review

--- JS_fundamentals/closure.js
+++ JS_fundamentals/closure.js
@@ -8,7 +8,8 @@
 // 4. function passed in as a callback  
 // 5. function is assigned inside a function and invoked outside that function
 
-//                                                                                               EX 1
+
+// USE CASE 1                                                                                               EX 1
 function foo() {
   var a = 2;
 
@@ -45,10 +46,11 @@ var baz = foo();
 // That inner scope is in fact still "in use", and thus does not go away.  
 // Who's using it? The function bar() itself.
 
+// Whatever facility we use to transport an inner function outside of its lexical scope, it will maintain 
+// a scope reference to where it was originally declared, and wherever we execute it, 
+// that closure will be exercised.
+
 //===================================================================================================================
-
-// USE CASE 1
-
 // USE CASE 2 - SetTimeout
 function wait(message) {
 
@@ -60,15 +62,26 @@ function wait(message) {
 
 // wait("Hello, closure!");
 
+// timer has a closure over the scope of wait.
+// After we call wait the scope should be gone.  But 100MS later timer still has access to it.
+
+
 //===================================================================================================================
 // Event Handlers -jQuery ect
+function setupBot(name, selector) {
+  $(selector).click(function activator() {
+    console.log("Activating: " + name);
+  });
+}
+
+// setupBot("Closure Bot 1", "#bot_1");
+// setupBot("Closure Bot 2", "#bot_2");
+
 // essentially whenever and wherever you treat functions(which access their own respective lexical scopes) 
 // as first - class values and pass them around, you are likely to see those functions exercising closure.
 // Be that timers, event handlers, Ajax requests, cross - window messaging, web workers, or any of the other 
 // asynchronous(or synchronous!) tasks, when you pass in a callback function, get ready to sling some closure 
 // around!
-
-
 
 //===================================================================================================================
 //                                                                                                    IN LOOPS                             
@@ -113,13 +126,12 @@ function wait(message) {
 //                                                                              prints correctly
 // EXPLAINATION:  let defines i in the block scope  + closure
 
-// let highjacks a block and declares a variable in the block
+// let FOR-LOOP behavior: the variable will be declared @ each iteration of the loop!
 // for (let i =0; i< 5; i++) {
 //   setTimeout(function timer(){
 //     console.log(i)
 //   },100);
 // }
-
 
 // ====================================================================================================
 //                                          MODULE
@@ -159,8 +171,6 @@ let singleTon = coolModule();
 // singleTon.doSomething();
 // singleTon.doAnother();
 
-
-
 let exampleModule = ((id) => {
   let stateID = () => {
     console.log(id)
@@ -181,6 +191,58 @@ let exampleModule = ((id) => {
 
 })('someguy');
 
-exampleModule.stateID();
-exampleModule.changeID('buddy');
-exampleModule.stateID();
+// exampleModule.stateID();
+// exampleModule.changeID('buddy');
+// exampleModule.stateID();
+
+//========================================================================================================
+// PRACTICE
+//========================================================================================================
+// ADD TO
+
+const addTo = (num1) => {
+  return (num2) => {
+    return num1 + num2;
+  }
+}
+
+// function addTo_A(num1) {
+//   function inner(num2) {
+//     return num1 + num2;
+//   }
+//   return inner;
+// }
+
+let addFive = addTo(5);
+// console.log(
+//   addFive(7)
+// );
+
+//================================================================================================
+// private methods - MODULE pattern
+
+// Private methods aren't just useful for restricting access to code: they also provide a powerful way of 
+// managing your global namespace, keeping non-essential methods from cluttering up the public interface to your code.
+
+const counterFive = ((startNum) => {
+  let privateVal = startNum;
+  
+  function increaseBy(num) {
+    privateVal += num;
+  }
+  function getVal() {
+    return privateVal;
+  }
+  return {
+    increaseBy: increaseBy,
+    getVal: getVal,
+  }
+})(5);
+
+counterFive.increaseBy(20);
+
+console.log(
+  counterFive.getVal()
+)
+
+
