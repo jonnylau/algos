de4beb61fe47d2d6436b803cd30107f3a8caddb4
Jon Lau <jonathanclau8@gmail.com>
2018-03-22 23:59:29 -0700

    prefix

---
+++ strings/prefix.js
@@ -0,0 +1,5 @@
+// iterate through the array of letters once building an array as you go
+// i @ 0 j@ 1
+// if they match, add 1 immediately to the left output array pos
+  // advance j and i
+// otherwise reset i = output[i - 1] and test again
